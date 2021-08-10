fd2d98d15f841786d25bbaaec0547d13dc161c3e
Jun Kurihara <kurihara@ieee.org>
2021-08-10 17:33:59 +0900

    fix entrypoint

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -8,7 +8,7 @@ source /opt/doh-proxy/etc/.env
 echo "doh-proxy: upstream dns server address: ${UPSTREAM_ADDR}:${UPSTREAM_PORT}"
 
 /opt/doh-proxy/sbin/doh-proxy \
-  --hostname={HOSTNAME} \
+  --hostname=${HOSTNAME} \
   --server-address=${UPSTREAM_ADDR}:${UPSTREAM_PORT} \
   --listen-address=0.0.0.0:3000 \
   --path=/dns-query \
