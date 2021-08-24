80a85fa275e3b53eaa29a096238948a5dd3d069e
Jun Kurihara <kurihara@ieee.org>
2021-08-24 19:24:07 +0900

    changed pattern matcher from HashSet to cedarwood

--- src/libdoh/src/plugin_block_domains.rs
+++ src/libdoh/src/plugin_block_domains.rs
@@ -1,21 +1,24 @@
 use crate::constants::*;
 use crate::log::*;
-use crate::utils::RequestQueryKey;
+use crate::utils::{reverse_string, RequestQueryKey};
+use cedarwood::Cedar;
 use regex::Regex;
-use std::collections::HashSet;
 
 #[derive(Debug, Clone)]
 pub struct DomainBlockRule {
-  pub domains: HashSet<String>,
+  prefix_cedar: Cedar,
+  suffix_cedar: Cedar,
+  prefix_dict: Vec<String>,
+  suffix_dict: Vec<String>,
 }
 
 impl DomainBlockRule {
   pub fn new(vec_domain_str: Vec<&str>) -> DomainBlockRule {
-    // TODO: currently either one of prefix or suffix match with '*' is supported
-    // TODO: Change to Patricia Trie data structure
     let start_with_star = Regex::new(r"^\*\..+").unwrap();
+    let end_with_star = Regex::new(r".+\.\*$").unwrap();
+    // TODO: currently either one of prefix or suffix match with '*' is supported
     let re = Regex::new(&format!("{}{}{}", r"^", REGEXP_DOMAIN_OR_PREFIX, r"$")).unwrap(); // TODO: TODO:
-    let hs: HashSet<String> = vec_domain_str
+    let dict: Vec<String> = vec_domain_str
       .iter()
       .map(|d| {
         if start_with_star.is_match(d) {
@@ -27,8 +30,94 @@ impl DomainBlockRule {
       .filter(|x| re.is_match(x) || (x.split('.').collect::<Vec<&str>>().len() == 1))
       .map(|y| y.to_string())
       .collect();
+    let prefix_dict: Vec<String> = dict
+      .iter()
+      .filter(|d| end_with_star.is_match(d))
+      .map(|d| d[..d.len() - 2].to_string())
+      .collect();
+    let suffix_dict: Vec<String> = dict
+      .iter()
+      .filter(|d| !end_with_star.is_match(d))
+      .map(|d| reverse_string(d))
+      .collect();
+    let ref_vec: Vec<&str> = prefix_dict.iter().map(AsRef::as_ref).collect();
+    let ref_rev_vec: Vec<&str> = suffix_dict.iter().map(AsRef::as_ref).collect();
 
-    DomainBlockRule { domains: hs }
+    let prefix_kv: Vec<(&str, i32)> = ref_vec
+      .into_iter()
+      .enumerate()
+      .map(|(k, s)| (s, k as i32))
+      .collect();
+    let mut prefix_cedar = Cedar::new();
+    prefix_cedar.build(&prefix_kv);
+
+    let suffix_kv: Vec<(&str, i32)> = ref_rev_vec
+      .into_iter()
+      .enumerate()
+      .map(|(k, s)| (s, k as i32))
+      .collect();
+    let mut suffix_cedar = Cedar::new();
+    suffix_cedar.build(&suffix_kv);
+
+    DomainBlockRule {
+      prefix_cedar,
+      suffix_cedar,
+      prefix_dict,
+      suffix_dict,
+    }
+  }
+
+  fn find_suffix_match(&self, query_domain: &str) -> bool {
+    let rev_nn = reverse_string(query_domain);
+    let matched_items = self
+      .suffix_cedar
+      .common_prefix_iter(&rev_nn)
+      .map(|(x, _)| self.suffix_dict[x as usize].clone());
+
+    let mut matched_as_domain = matched_items.filter(|found| {
+      if found.len() == rev_nn.len() {
+        true
+      } else {
+        if let Some(nth) = rev_nn.chars().nth(found.chars().count()) {
+          if nth.to_string() == "." {
+            true
+          } else {
+            false
+          }
+        } else {
+          false
+        }
+      }
+    });
+    if let Some(_) = matched_as_domain.next() {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  fn find_prefix_match(&self, query_domain: &str) -> bool {
+    let matched_items = self
+      .prefix_cedar
+      .common_prefix_iter(query_domain)
+      .map(|(x, _)| self.prefix_dict[x as usize].clone());
+
+    let mut matched_as_domain = matched_items.filter(|found| {
+      if let Some(nth) = query_domain.chars().nth(found.chars().count()) {
+        if nth.to_string() == "." {
+          true
+        } else {
+          false
+        }
+      } else {
+        false
+      }
+    });
+    if let Some(_) = matched_as_domain.next() {
+      return true;
+    } else {
+      return false;
+    }
   }
 
   pub fn should_block(&self, q_key: &RequestQueryKey) -> bool {
@@ -46,31 +135,17 @@ impl DomainBlockRule {
       }
     }
 
-    // exact match
-    if self.domains.contains(&nn) {
-      info!("domain blocked!: {}", nn);
+    if self.find_suffix_match(&nn) {
+      debug!("[with cw] suffix/exact match found: {}", nn);
       return true;
     }
 
-    let nn_part: Vec<&str> = nn.split('.').collect();
-    let parts_num = nn_part.len();
-    if parts_num > 1 {
-      for i in 1..parts_num {
-        let suffix = nn_part[i..parts_num].join(".");
-        let prefix = nn_part[0..parts_num - i].join(".");
-        if self.domains.contains(&suffix) {
-          debug!("domain suffix blocked!: {}", nn);
-          return true;
-        }
-
-        if self.domains.contains(&format!("{}.*", prefix)) {
-          debug!("domain prefix blocked!: {}", nn);
-          return true;
-        }
-      }
-
-      // TODO: other matching patterns
+    if self.find_prefix_match(&nn) {
+      debug!("[with cw] prefix match found: {}", nn);
+      return true;
     }
+
+    // TODO: other matching patterns
 
     false
   }
--- src/libdoh/src/utils.rs
+++ src/libdoh/src/utils.rs
@@ -126,3 +126,7 @@ impl TryFrom<&Message> for RequestKey {
     }
   }
 }
+
+pub fn reverse_string(text: &str) -> String {
+  text.chars().rev().collect::<String>()
+}
